f76b1031f16b61eae23f9d37e7627c067a4b8962
Harsh Zalavadiya <harsh@strandls.com>
2019-07-04 16:55:39 +0530

    refactor: ☕ report → formik

--- src/components/union/lot/cupping/index.tsx
+++ src/components/union/lot/cupping/index.tsx
@@ -1,16 +1,12 @@
 import { Button } from "carbon-components-react";
+import { Field, Formik } from "formik";
 import { observer } from "mobx-react";
 import React, { Component } from "react";
-import {
-  FieldGroup,
-  FormBuilder,
-  Validators,
-  FieldControl,
-} from "react-reactive-form";
+import * as Yup from "yup";
 
+import { textInput } from "/@components/@core/formik";
 import { QualityStore } from "/@stores/qr.store";
 import { getToday } from "/@utils/basic";
-import { textInput, dateInput, numberInput } from "/@components/@core/form";
 
 interface IProps {
   lotId;
@@ -25,221 +21,264 @@ interface IProps {
 export default class CuppingComponent extends Component<IProps> {
   qualityStore = new QualityStore();
 
-  state = {
-    form: FormBuilder.group({
-      lotName: [this.props.lotName, Validators.required],
-      lotId: [this.props.lotId, Validators.required],
-      date: [getToday(), Validators.required],
-      cfa: [this.props.lotInfo.cfa, Validators.required],
-      ccCode: [this.props.lotInfo.cc_code, Validators.required],
+  cuppingForm = {
+    validationSchema: Yup.object().shape({
+      lotName: Yup.string().required(),
+      lotId: Yup.string().required(),
+      date: Yup.date().required(),
+      cfa: Yup.string().required(),
+      ccCode: Yup.string().required(),
 
-      cupper: [this.props.lotInfo.cupper, Validators.required],
-      sampleType: [this.props.type, Validators.required],
+      cupper: Yup.string().required(),
+      sampleType: Yup.string().required(),
 
       // Params
-      fragranceAroma: [0, Validators.required],
-      flavour: [0, Validators.required],
-      acidity: [0, Validators.required],
-      body: [0, Validators.required],
-      afterTaste: [0, Validators.required],
-      balance: [0, Validators.required],
-      sweetness: [0, Validators.required],
-      uniformity: [0, Validators.required],
-      cleanCup: [0, Validators.required],
-      overAll: [0, Validators.required],
+      fragranceAroma: Yup.number().required(),
+      flavour: Yup.number().required(),
+      acidity: Yup.number().required(),
+      body: Yup.number().required(),
+      afterTaste: Yup.number().required(),
+      balance: Yup.number().required(),
+      sweetness: Yup.number().required(),
+      uniformity: Yup.number().required(),
+      cleanCup: Yup.number().required(),
+      overAll: Yup.number().required(),
 
       // Problems
-      taint: [0, Validators.required],
-      fault: [0, Validators.required],
+      taint: Yup.number().required(),
+      fault: Yup.number().required(),
 
-      notes: ["", Validators.required],
+      notes: Yup.string().required(),
     }),
+    initialValues: {
+      lotName: this.props.lotName,
+      lotId: this.props.lotId,
+      date: getToday(),
+      cfa: this.props.lotInfo.cfa,
+      ccCode: this.props.lotInfo.cc_code,
+
+      cupper: this.props.lotInfo.cupper,
+      sampleType: this.props.type,
+
+      // Params
+      fragranceAroma: "",
+      flavour: "",
+      acidity: "",
+      body: "",
+      afterTaste: "",
+      balance: "",
+      sweetness: "",
+      uniformity: "",
+      cleanCup: "",
+      overAll: "",
+
+      // Problems
+      taint: "",
+      fault: "",
+
+      notes: "",
+    },
   };
 
-  gradeTotal = () => {
-    const _v = this.state.form.value;
-    return (
-      parseInt(_v.fragranceAroma.toString()) +
-      parseInt(_v.flavour.toString()) +
-      parseInt(_v.acidity.toString()) +
-      parseInt(_v.body.toString()) +
-      parseInt(_v.afterTaste.toString()) +
-      parseInt(_v.balance.toString()) +
-      parseInt(_v.sweetness.toString()) +
-      parseInt(_v.uniformity.toString()) +
-      parseInt(_v.cleanCup.toString()) +
-      parseInt(_v.overAll.toString()) -
-      (parseInt(_v.taint.toString()) + parseInt(_v.fault.toString()))
-    );
+  gradeTotal = v => {
+    const _t =
+      v.fragranceAroma +
+      v.flavour +
+      v.acidity +
+      v.body +
+      v.afterTaste +
+      v.balance +
+      v.sweetness +
+      v.uniformity +
+      v.cleanCup +
+      v.overAll -
+      (v.taint + v.fault);
+    return typeof _t === "number" ? _t : "NaN";
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
+  handleSubmit = (values, actions) => {
+    actions.setSubmitting(false);
+    console.log(values);
     this.qualityStore.createCuppingReport({
-      ...this.state.form.value,
+      ...values,
       timestamp: new Date().getTime(),
     });
   };
 
-  renderFieldGroup = ({ get, invalid }) => {
-    return (
-      <form className="bx--form" onSubmit={this.handleSubmit}>
-        <h3 className="eco--form-title">Lot Information</h3>
-        <div className="bx--row">
-          <div className="bx--col-lg-3 bx--col-sm-12">
-            <FieldControl
-              name="lotName"
-              render={textInput}
-              meta={{ label: "Lot Name", readOnly: true }}
-            />
-          </div>
-          <div className="bx--col-lg-3 bx--col-sm-12">
-            <FieldControl
-              name="date"
-              render={dateInput}
-              meta={{ label: "Cupping Date", readOnly: true }}
-            />
-          </div>
-          <div className="bx--col-lg-3 bx--col-sm-12">
-            <FieldControl
-              name="cfa"
-              render={textInput}
-              meta={{ label: "CFA", readOnly: true }}
-            />
-          </div>
-          <div className="bx--col-lg-3 bx--col-sm-12">
-            <FieldControl
-              name="ccCode"
-              render={textInput}
-              meta={{ label: "CC Code", readOnly: true }}
-            />
-          </div>
-
-          <div className="bx--col-lg-3 bx--col-sm-12">
-            <FieldControl
-              name="sampleType"
-              render={textInput}
-              meta={{ label: "Sample Type" }}
-            />
-          </div>
+  renderGreenForm = ({ handleSubmit, isValid, values }) => (
+    <form className="bx--form" onSubmit={handleSubmit}>
+      <h3 className="eco--form-title">Lot Information</h3>
+      <div className="bx--row">
+        <div className="bx--col-lg-3 bx--col-sm-12">
+          <Field
+            label="Lot Name"
+            name="lotName"
+            component={textInput}
+            readOnly={true}
+          />
+        </div>
+        <div className="bx--col-lg-3 bx--col-sm-12">
+          <Field
+            label="Cupping Date"
+            name="date"
+            component={textInput}
+            type="date"
+            readOnly={true}
+          />
+        </div>
+        <div className="bx--col-lg-3 bx--col-sm-12">
+          <Field
+            label="Cooperative"
+            name="cfa"
+            component={textInput}
+            readOnly={true}
+          />
+        </div>
+        <div className="bx--col-lg-3 bx--col-sm-12">
+          <Field
+            label="Origin"
+            name="ccCode"
+            component={textInput}
+            readOnly={true}
+          />
         </div>
 
-        <h3 className="eco--form-title">Qualities</h3>
-        <div className="bx--row">
-          <div className="bx--col-lg-3 bx--col-sm-12">
-            <FieldControl
-              name="fragranceAroma"
-              render={numberInput}
-              meta={{ label: "Fragrance Aroma" }}
-            />
-          </div>
-          <div className="bx--col-lg-3 bx--col-sm-12">
-            <FieldControl
-              name="flavour"
-              render={numberInput}
-              meta={{ label: "Flavour" }}
-            />
-          </div>
-          <div className="bx--col-lg-3 bx--col-sm-12">
-            <FieldControl
-              name="acidity"
-              render={numberInput}
-              meta={{ label: "Acidity" }}
-            />
-          </div>
-          <div className="bx--col-lg-3 bx--col-sm-12">
-            <FieldControl
-              name="body"
-              render={numberInput}
-              meta={{ label: "Body" }}
-            />
-          </div>
-          <div className="bx--col-lg-3 bx--col-sm-12">
-            <FieldControl
-              name="afterTaste"
-              render={numberInput}
-              meta={{ label: "After Taste" }}
-            />
-          </div>
-          <div className="bx--col-lg-3 bx--col-sm-12">
-            <FieldControl
-              name="balance"
-              render={numberInput}
-              meta={{ label: "Balance" }}
-            />
-          </div>
-          <div className="bx--col-lg-3 bx--col-sm-12">
-            <FieldControl
-              name="sweetness"
-              render={numberInput}
-              meta={{ label: "Sweetness" }}
-            />
-          </div>
-          <div className="bx--col-lg-3 bx--col-sm-12">
-            <FieldControl
-              name="uniformity"
-              render={numberInput}
-              meta={{ label: "Uniformity" }}
-            />
-          </div>
-          <div className="bx--col-lg-3 bx--col-sm-12">
-            <FieldControl
-              name="cleanCup"
-              render={numberInput}
-              meta={{ label: "Clean Cup" }}
-            />
-          </div>
-          <div className="bx--col-lg-3 bx--col-sm-12">
-            <FieldControl
-              name="overAll"
-              render={numberInput}
-              meta={{ label: "Overall" }}
-            />
-          </div>
+        <div className="bx--col-lg-3 bx--col-sm-12">
+          <Field
+            label="Sample Type"
+            name="sampleType"
+            component={textInput}
+            readOnly={true}
+          />
         </div>
+      </div>
 
-        <div className="bx--row">
-          <div className="bx--col-lg-6 bx--col-sm-12">
-            <h3 className="eco--form-title">Problems</h3>
-            <div className="bx--row">
-              <div className="bx--col-lg-6 bx--col-sm-12">
-                <FieldControl
-                  name="taint"
-                  render={numberInput}
-                  meta={{ label: "Taint" }}
-                />
-              </div>
-              <div className="bx--col-lg-6 bx--col-sm-12">
-                <FieldControl
-                  name="fault"
-                  render={numberInput}
-                  meta={{ label: "Fault" }}
-                />
-              </div>
+      <h3 className="eco--form-title">Qualities</h3>
+      <div className="bx--row">
+        <div className="bx--col-lg-3 bx--col-sm-12">
+          <Field
+            label="Fragrance Aroma"
+            name="fragranceAroma"
+            component={textInput}
+            type="number"
+          />
+        </div>
+        <div className="bx--col-lg-3 bx--col-sm-12">
+          <Field
+            label="Flavour"
+            name="flavour"
+            component={textInput}
+            type="number"
+          />
+        </div>
+        <div className="bx--col-lg-3 bx--col-sm-12">
+          <Field
+            label="Acidity"
+            name="acidity"
+            component={textInput}
+            type="number"
+          />
+        </div>
+        <div className="bx--col-lg-3 bx--col-sm-12">
+          <Field label="Body" name="body" component={textInput} type="number" />
+        </div>
+        <div className="bx--col-lg-3 bx--col-sm-12">
+          <Field
+            label="After Taste"
+            name="afterTaste"
+            component={textInput}
+            type="number"
+          />
+        </div>
+        <div className="bx--col-lg-3 bx--col-sm-12">
+          <Field
+            label="Balance"
+            name="balance"
+            component={textInput}
+            type="number"
+          />
+        </div>
+        <div className="bx--col-lg-3 bx--col-sm-12">
+          <Field
+            label="Sweetness"
+            name="sweetness"
+            component={textInput}
+            type="number"
+          />
+        </div>
+        <div className="bx--col-lg-3 bx--col-sm-12">
+          <Field
+            label="Uniformity"
+            name="uniformity"
+            component={textInput}
+            type="number"
+          />
+        </div>
+        <div className="bx--col-lg-3 bx--col-sm-12">
+          <Field
+            label="Clean Cup"
+            name="cleanCup"
+            component={textInput}
+            type="number"
+          />
+        </div>
+        <div className="bx--col-lg-3 bx--col-sm-12">
+          <Field
+            label="Overall"
+            name="overAll"
+            component={textInput}
+            type="number"
+          />
+        </div>
+      </div>
+
+      <div className="bx--row">
+        <div className="bx--col-lg-6 bx--col-sm-12">
+          <h3 className="eco--form-title">Problems</h3>
+          <div className="bx--row">
+            <div className="bx--col-lg-6 bx--col-sm-12">
+              <Field
+                label="Taint"
+                name="taint"
+                component={textInput}
+                type="number"
+              />
+            </div>
+            <div className="bx--col-lg-6 bx--col-sm-12">
+              <Field
+                label="Fault"
+                name="fault"
+                component={textInput}
+                type="number"
+              />
             </div>
           </div>
-          <div className="bx--col-lg-6 bx--col-sm-12">
-            <h3 className="eco--form-title">Additional Information</h3>
-            <FieldControl
-              name="notes"
-              render={textInput}
-              meta={{ label: "Notes (comma seprated)" }}
-            />
-          </div>
         </div>
+        <div className="bx--col-lg-6 bx--col-sm-12">
+          <h3 className="eco--form-title">Additional Information</h3>
+          <Field
+            label="Notes (comma seprated)"
+            name="notes"
+            component={textInput}
+          />
+        </div>
+      </div>
 
-        <h3 className="eco--form-title">TT - {this.gradeTotal()}</h3>
+      <h3 className="eco--form-title">TT - {this.gradeTotal(values)}</h3>
 
-        <Button type="submit" disabled={invalid}>
-          Submit
-        </Button>
-      </form>
-    );
-  };
+      <Button type="submit" disabled={!isValid}>
+        Submit
+      </Button>
+    </form>
+  );
 
   render() {
     return (
-      <FieldGroup control={this.state.form} render={this.renderFieldGroup} />
+      <Formik
+        {...this.cuppingForm}
+        onSubmit={this.handleSubmit}
+        render={this.renderGreenForm}
+      />
     );
   }
 }
