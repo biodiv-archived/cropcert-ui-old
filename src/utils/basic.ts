55620b11cad5aa92b40ef6fcbec0142fb75047f1
Harsh Zalavadiya <harsh@strandls.com>
2019-07-05 15:31:00 +0530

    refactor: GRN to uppercase fix

--- src/utils/basic.ts
+++ src/utils/basic.ts
@@ -36,7 +36,8 @@ export const camelCaseToStartCase = camelCase => {
     .replace(/([a-z])([A-Z])/g, "$1 $2")
     .replace(/([A-Z])([A-Z][a-z])/g, "$1 $2")
     .replace(/([a-z])([0-9])/gi, "$1 $2")
-    .replace(/([0-9])([a-z])/gi, "$1 $2");
+    .replace(/([0-9])([a-z])/gi, "$1 $2")
+    .replace("Grn", "GRN");
 };
 
 export const formattedTimeStamp = (d = new Date()) => {
